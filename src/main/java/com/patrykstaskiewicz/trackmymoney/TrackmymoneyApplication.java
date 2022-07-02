1e44abbed52e62f2b51005745cffc2363e97c843
Patryk Staśkiewicz <patryk.staskiewicz0@gmail.com>
2022-07-02 13:14:52 +0200

    setting up repository

---
+++ src/main/java/com/patrykstaskiewicz/trackmymoney/TrackmymoneyApplication.java
@@ -0,0 +1,13 @@
+package com.patrykstaskiewicz.trackmymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmymoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackmymoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patrykstaskiewicz/trackmymoney/TrackmymoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrykstaskiewicz.trackmymoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackmymoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
